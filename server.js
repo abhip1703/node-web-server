629d11a2b1ccadf765e861912be86898ae7f27fc
Abhishek Paradkar <abhishek@Abhisheks-MacBook-Air.local>
2018-06-21 12:00:51 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFileSync('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintanance.hbs')
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  next();
+});
+hbs.registerHelper('getCurrentYear', () => {
+
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (request, response) => {
+  response.render('home.hbs', {
+    pageTitle: 'Home page',
+    welcomeMessage: 'Hello there!'
+  })
+});
+
+app.get('/about', (request, response) => {
+  response.render('about.hbs', {
+    pageTitle: 'About page'
+  });
+});
+
+app.get('/bad', (request, response) => {
+  response.send({
+    errorMessage: 'Unable to complete request'
+  });
+});
+app.listen(3000, () => {
+  console.log('server is up on port 3000');
+});
